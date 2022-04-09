ce353d88a510eef1bf737943ab640827042c019e
Yuneeva Anastasya <anastasya.yuneeva@volvo.com>
2022-04-10 02:16:46 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,92 +1,50 @@
 import './App.css';
 import React, { useState } from "react";
-import styled from "styled-components";
+import { useEffect } from "react";
 
-const Button = styled.button`
-  float: right;
-  background: palevioletred;
-  color: #FFF;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  padding: 3px 10px;
-  outline: none;
-  cursor: pointer    
-`;
+ const list = [
+  "Ann",
+  "Alice",
+  "Harry",
+  "Lina",
+  "Katy",
+  "Lily"
+]
 
-const Input = styled.input`
-  width: 150px;
-  outline: none;
-  font-size: 13px;
-  padding-top: 7px;
-  padding-bottom: 7px;
-  padding-left: 10px;
-`;
+function Filter(props) {
 
-const Div = styled.div`
-width: 250px;
-height: 50px;
-position : relative ; 
-background : white ; border-radius : 5 px ;   
-margin : 0  auto ; 
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-position: relative;
-`;
+  const [filterList, setFilterList] = useState([]);
 
-const Header = styled.h1`
-  margin: 0;
-  text-align: center;
-  font-size: 36px;
-  color: #343a40;
-`;
+   useEffect(() => {
+   setFilterList(props.items);
+}, [props.items]);
 
-function Todos() {
-  const [todos, setTodo] = useState([{ name: "Buy apple" }]);
-  const [newTodo, setnewTodo] = useState("");
+  const Search = (event) => {
 
-  const add = todo => setTodo([...todos, todo]);
-
-  const remove = index => {
-    setTodo([...todos.slice(0, index), ...todos.slice(index + 1)]);
-  };
-  
-  const handleAddClick = () => {
-    
-    if (newTodo === "") {
+    if (event.target.value === "") {
+      setFilterList(props.items);
       return;
     }
-    
-    add({ name: newTodo });
-    setnewTodo("");
-  };
 
+    const foundValues = props.items.filter(
+      (item) => 
+        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1 
+    );
+
+    setFilterList(foundValues);
+  };
+    
   return (
     <>
-      <Div className="add-todo">
-        <Input
-          type="text"
-          value={newTodo}
-          onChange={event => setnewTodo(event.target.value)}
-        />
-        <Button onClick={handleAddClick}>Add todo</Button>
-      </Div>
-      <div className="todos">
-        {todos.map((todo, index) => {
-          return <Todo todo={todo} onRemove={() => remove(index)} />;
-        })}
-      </div>
-   </>
-  );
-}
-
-function Todo({ todo, Remove }) {
-  return (
-    <Div className="todo">
-      <span>{todo.name}</span>
-      <Button onClick={Remove}>Remove</Button>
-    </Div>
+     <div>
+       Search: <input name="query" type="text" onChange={Search} />
+     </div>
+     {
+       filterList.map((item) => (
+         <div className="item">{item}</div>
+       ))
+     }   
+    </>
   );
 }
 
@@ -96,8 +54,7 @@ function App() {
   return (
     <div className="App">
       <div className="App">
-        <Header>ToDo List</Header>
-        <Todos />
+        <Filter items={list} />
       </div>
     </div>
   );
